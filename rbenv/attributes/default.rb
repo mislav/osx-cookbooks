3f01fc3113d788f35b06d41af7f8a7a64ae6a850
Joshua Peek <josh@joshpeek.com>
2012-08-22 12:55:50 -0500

    Set default global rbenv

--- rbenv/attributes/default.rb
+++ rbenv/attributes/default.rb
@@ -1,3 +1,4 @@
 default.rbenv[:user]     = ENV['SUDO_USER'] || ENV['USER']
 default.rbenv[:root]     = ENV['RBENV_ROOT'] || "#{ENV['HOME']}/.rbenv"
 default.rbenv[:versions] = []
+default.rbenv[:global]   = "1.9.3-p194"
