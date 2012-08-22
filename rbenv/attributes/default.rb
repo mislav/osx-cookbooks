8cab6db845edba49e448aaed847ed863f0b5526c
Joshua Peek <josh@joshpeek.com>
2012-08-22 12:56:30 -0500

    Set my prefered rbenv root

--- rbenv/attributes/default.rb
+++ rbenv/attributes/default.rb
@@ -1,4 +1,5 @@
 default.rbenv[:user]     = ENV['SUDO_USER'] || ENV['USER']
-default.rbenv[:root]     = ENV['RBENV_ROOT'] || "#{ENV['HOME']}/.rbenv"
+# default.rbenv[:root]     = ENV['RBENV_ROOT'] || "#{ENV['HOME']}/.rbenv"
+default.rbenv[:root]     = "/usr/local/lib/rbenv"
 default.rbenv[:versions] = []
 default.rbenv[:global]   = "1.9.3-p194"
