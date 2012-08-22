8a1196d7d73df73d7456ff69731daaea16f90511
Joshua Peek <josh@joshpeek.com>
2012-08-22 16:37:35 -0500

    Ensure ssh keys exist

--- ssh/recipes/default.rb
+++ ssh/recipes/default.rb
@@ -1,3 +1,13 @@
+execute "ensure ssh private key exist" do
+  command "sh -c 'exit 1'"
+  creates "#{node[:ssh][:path]}/id_rsa"
+end
+
+execute "ensure ssh public key exist" do
+  command "sh -c 'exit 1'"
+  creates "#{node[:ssh][:path]}/id_rsa.pub"
+end
+
 file "#{node[:ssh][:path]}/id_rsa" do
   mode '0600'
 end
