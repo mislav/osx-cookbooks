45c214f90892046dadd64d6e6a221dc4cbb386a0
Joshua Peek <josh@joshpeek.com>
2012-09-01 14:05:15 -0500

    Double check CLT and Xcode compilers are the same

--- xcode/recipes/default.rb
+++ xcode/recipes/default.rb
@@ -12,3 +12,13 @@ execute "sudo xcode-select -switch #{node[:xcode][:path]}" do
   not_if { `xcode-select -print-path`.chomp == node[:xcode][:path] }
   user node[:xcode][:user]
 end
+
+execute "ensure Xcode llvm-gcc version is the same as Command Line Tools" do
+  command "sh -c 'exit 1'"
+  not_if { `llvm-gcc --version` == `xcrun llvm-gcc --version` }
+end
+
+execute "ensure Xcode clang version is the same as Command Line Tools" do
+  command "sh -c 'exit 1'"
+  not_if { `clang --version` == `xcrun clang --version` }
+end
