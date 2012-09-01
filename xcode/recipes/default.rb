37e9b84a3eabe8a343b5ef9a0734d52ff66e638e
Joshua Peek <josh@joshpeek.com>
2012-09-01 18:07:52 -0500

    Check for llvm-gcc

--- xcode/recipes/default.rb
+++ xcode/recipes/default.rb
@@ -3,14 +3,19 @@ execute "ensure Xcode is installed" do
   creates "/Applications/Xcode.app"
 end
 
+execute "sudo xcode-select -switch #{node[:xcode][:path]}" do
+  not_if { `xcode-select -print-path`.chomp == node[:xcode][:path] }
+  user node[:xcode][:user]
+end
+
 execute "ensure Xcode license is accepted" do
   command "xcrun -find git"
   not_if "xcrun -find git"
 end
 
-execute "sudo xcode-select -switch #{node[:xcode][:path]}" do
-  not_if { `xcode-select -print-path`.chomp == node[:xcode][:path] }
-  user node[:xcode][:user]
+execute "ensure Xcode Command Line Tools are installed" do
+  command "which llvm-gcc"
+  not_if "which llvm-gcc"
 end
 
 execute "ensure Xcode llvm-gcc version is the same as Command Line Tools" do
